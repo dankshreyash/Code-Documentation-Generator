7eeb22eaf5142317932ba6b5e566ce3685c99025
dankshreyash <shreyashgaikwad27@gmail.com>
2026-07-20 21:02:15 +0530

    Update API URL to Render deployment

--- Frontend/services/api.js
+++ Frontend/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://code-documentation-generator.onrender.com";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
